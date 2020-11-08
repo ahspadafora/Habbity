144d5d1525d112f85c907fe6b33b9f8e3d76fdda
Amber Spadafora <ahelenespadafora@gmail.com>
2020-11-08 04:17:52 -0500

    Initial Commit

---
+++ Habitty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habitty
+//
+//  Created by Amber Spadafora on 11/8/20.
+//  Copyright © 2020 Amber Spadafora. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
